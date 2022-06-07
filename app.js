c58063206cc5e8ff8821acb61162f40007d5ca0f
Shixiang Yang <adam_yang2021@163.com>
2022-06-06 21:20:36 -0700

    merge branch

--- app.js
+++ app.js
@@ -1,56 +1,89 @@
 // app.js
-import request from './pages/request/request'
+import request from "./pages/request/request";
 // let app = getApp();
 App({
-  async onLaunch() {
-    let that = this;
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    // 登录
-    wx.login({
-      success: async res => {
-        let code = res.code;
-        let result = await request('/getOpenId', { code });
-        that.globalData.openid = result;
-        // 获取用户openid后需要看是否第一次登陆
-        let customerInfo = await request(`/personal/openid/${that.globalData.openid}`,{});
-        that.globalData.isFirst = customerInfo.length == 0 ? true : false;
-        // that.globalData.isFirst = customerInfo.length == 0 ? true : false;
-        // 若不是，获取用户喜好并跳转页面
-        if(!that.globalData.isFirst){
-          let temp = await request(`/personal/openid/${that.globalData.openid}`, {}, 'GET');
-          let tempUserInfo = JSON.parse(JSON.stringify(temp[0]));
-          for (let i in tempUserInfo) {
-            if (tempUserInfo[i] == 1) {
-              let index = that.globalData.typesEng.indexOf(i);
-              let type = that.globalData.types[index];
-              that.globalData.selecedArr[index] = 1;
-              that.globalData.userPreference.push(type);
-              that.globalData.userPreferenceEng.push(i);
-            }
-          }
-        }
-        // 若是第一次登陆，跳转到欢迎界面
-        else{
-          wx.redirectTo({
-            url: '/pages/welcome/welcome',
-          })
-        }
-        // console.log(that.globalData.userPreference)
-      }
-    })
-  },
-  globalData: {
-    isFirst: false,
-    userInfo: {},
-    openid: 0,
-    userPreference: [],
-    userPreferenceEng: [],
-    types: ['鸡肉', '蛋类', '乳制品', '牛肉', '鱼类', '豆制品', '猪肉', '贝类','谷物类','坚果','花生', '芝麻'],
-    typesEng: ['chicken', 'eggs', 'dairy', 'beef', 'fish', 'soy', 'pork', 'shellfish',
-    'wheat_gluten', 'tree_nuts', 'peanuts', 'sesame'],
-    selecedArr: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  }
-})
+	async onLaunch() {
+		let that = this;
+		// 展示本地存储能力
+		const logs = wx.getStorageSync("logs") || [];
+		logs.unshift(Date.now());
+		wx.setStorageSync("logs", logs);
+		// 登录
+		wx.login({
+			success: async (res) => {
+				let code = res.code;
+				let result = await request("/getOpenId", { code });
+				that.globalData.openid = result;
+				// 获取用户openid后需要看是否第一次登陆
+				let customerInfo = await request(
+					`/personal/openid/${that.globalData.openid}`,
+					{}
+				);
+				that.globalData.isFirst = customerInfo.length == 0 ? true : false;
+				// that.globalData.isFirst = customerInfo.length == 0 ? true : false;
+				// 若不是，获取用户喜好并跳转页面
+				if (!that.globalData.isFirst) {
+					let temp = await request(
+						`/personal/openid/${that.globalData.openid}`,
+						{},
+						"GET"
+					);
+					let tempUserInfo = JSON.parse(JSON.stringify(temp[0]));
+					for (let i in tempUserInfo) {
+						if (tempUserInfo[i] == 1) {
+							let index = that.globalData.typesEng.indexOf(i);
+							let type = that.globalData.types[index];
+							that.globalData.selecedArr[index] = 1;
+							that.globalData.userPreference.push(type);
+							that.globalData.userPreferenceEng.push(i);
+						}
+					}
+				}
+				// 若是第一次登陆，跳转到欢迎界面
+				else {
+					wx.redirectTo({
+						url: "/pages/welcome/welcome",
+					});
+				}
+				// console.log(that.globalData.userPreference)
+			},
+		});
+	},
+	globalData: {
+		isFirst: false,
+		userInfo: {},
+		openid: 0,
+		userPreference: [],
+		userPreferenceEng: [],
+		types: [
+			"鸡肉",
+			"蛋类",
+			"乳制品",
+			"牛肉",
+			"鱼类",
+			"豆制品",
+			"猪肉",
+			"贝类",
+			"谷物类",
+			"坚果",
+			"花生",
+			"芝麻",
+		],
+		typesEng: [
+			"chicken",
+			"eggs",
+			"dairy",
+			"beef",
+			"fish",
+			"soy",
+			"pork",
+			"shellfish",
+			"wheat_gluten",
+			"tree_nuts",
+			"peanuts",
+			"sesame",
+		],
+		selecedArr: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		mealInterval: ["Breakfast", "Lunch", "Dinner"],
+	},
+});
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,7 @@
 // pages/home/home.js
 import request from '../request/request'
 var app = getApp()
+
 Page({
 
   /**
@@ -9,7 +10,15 @@ Page({
   data: {
     userInfo: {},
     openid: 0,
-    isFirst: true
+    myDate: '0000-00-00',
+    isFirst: true,
+    isWeekDay: true,
+    isBreakfast: false,
+    isLunch: false,
+    isDinner: false,
+    vlgOpen: true,
+    evkOpen: true,
+    pksOpen: true,
   },
   // 首页登陆
   homeLogin() {
@@ -46,6 +55,7 @@ Page({
       }
     })
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -67,8 +77,71 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    let date = new Date();
+    let myDay = date.getDay();
+    let myTime = date.toTimeString();
+    let myDate = date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate();
+    // 判断是否工作日
+    if(myDay>=1 && myDay <=5){
+      this.setData({
+        isWeekDay: true,
+      })
+    }
+    // 判断是否开门
+    if(myTime >= '21:00'){
+      this.setData({
+        vlgOpen: true,
+        evkOpen: false,
+        pksOpen: false,
+      })
+    }
+    if(myTime >= '22:00' || myTime <= "07:00"){
+      this.setData({
+        vlgOpen: false,
+        evkOpen: false,
+        pksOpen: false,
+      })
+    }
+    if(myTime >= '07:00' && myTime <= '10:30'){
+      this.setData({
+        vlgOpen: true,
+        evkOpen: true,
+        pksOpen: true,
+      })
+    }
+    if(myTime >= '10:30' && myTime <= '11:00'){
+      this.setData({
+        vlgOpen: false,
+        evkOpen: false,
+        pksOpen: false,
+      })
+    }
+    // 判断mealtime
+    if(myTime <= '10:30' && myTime >= '07:00'){
+      this.setData({
+        isBreakfast: true,
+        isLunch: false,
+        isDinner: false,
+      })
+    }
+    if(myTime <= '16:00' && myTime >= '10:30'){
+      this.setData({
+        isBreakfast: false,
+        isLunch: true,
+        isDinner: false,
+      })
+    }
+    if(myTime <= '22:00' && myTime >= '16:00'){
+      this.setData({
+        isBreakfast: false,
+        isLunch: false,
+        isDinner: true,
+      })
+    }
+    // 初始化数据 
     this.setData({
       openid: app.globalData.openid,
+      myDate: myDate,
     })
   },
 
